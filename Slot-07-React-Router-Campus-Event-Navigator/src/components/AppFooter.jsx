3bc68f4600971a33dcfa629566a2fb05683ed9d9
Vo Anh Hao <hao.ce191463@gmail.com>
2026-09-17 16:17:04 +0700

    Delete Slot-07-React-Router-Campus-Event-Navigator/src directory

--- Slot-07-React-Router-Campus-Event-Navigator/src/components/AppFooter.jsx
+++
@@ -1,9 +0,0 @@
-function AppFooter() {
-    return (
-        <footer className="app-footer">
-            SBA301 • Campus Event Navigator • React Router Practice
-        </footer>
-    );
-}
-
-export default AppFooter;
